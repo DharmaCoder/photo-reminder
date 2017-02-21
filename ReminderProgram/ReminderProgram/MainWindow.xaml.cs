086a737e2fbbf5c9ebeebb6342a746d4e8b595e0
Hussean <fantominitiative@gmail.com>
2017-02-21 06:35:00 -0800

    Add files via upload

---
+++ ReminderProgram/ReminderProgram/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32; // for open file dialog
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReminderProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //TODO - add sound effect option
+        //TODO - add options for time intervals
+        //TODO - Add a remove phrases Button
+        //TODO - clean up garbage code
+
+        private string imagePath;
+        public string ImagePath
+        {
+            get
+            {
+                return imagePath;
+            }
+            set
+            {
+                ImagePath = imagePath;
+            }
+        }
+
+        //list of phrases from listbox
+        private List<string> phrases = new List<string>();
+        public List<string> Phrases
+        {
+            get
+            {
+                return phrases;
+            }
+            set
+            {
+                phrases = Phrases;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //adds user phrases to listbox and saves listbox items to phrases list
+        private void addPhraseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (addPhraseTextBox.Text != "")
+            {
+                phrasesListBox.Items.Add(addPhraseTextBox.Text);
+                phrases.Add(addPhraseTextBox.Text);
+                addPhraseTextBox.Clear();
+                addPhraseTextBox.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Please type a phrase!");
+            }
+        }
+
+        private void addPictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFD = new OpenFileDialog();
+                openFD.Filter = "ImageFiles (*.bmp; *.jpg; *.jpeg,*.png)| *.BMP; *.JPG; *.JPEG; *.PNG";
+                if (openFD.ShowDialog() == true)
+                {
+                    imagePath = openFD.FileName;
+                    imagePathLabel.Content = imagePath;
+                    userPhotoBox.Source = new BitmapImage(new Uri(imagePath));
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (imagePath != null && phrases.Count != 0)
+            {
+                ReminderWindow reminderWindow = new ReminderWindow();
+                reminderWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Yeah quit trying to break shit.");
+            }
+        }
+    }
+}
---
+++ ReminderProgram/ReminderProgram/ReminderWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ReminderProgram
+{
+    /// <summary>
+    /// Interaction logic for ReminderWindow.xaml
+    /// </summary>
+    public partial class ReminderWindow : Window
+    {
+        //TODO - make timespans customizable maybe?
+
+        //short timespans for testing purposes - change later
+        private TimeSpan isShowing = TimeSpan.FromSeconds(3);
+        private TimeSpan isHidden = TimeSpan.FromSeconds(3);
+
+        private int currentPhrase = 10;
+        private DispatcherTimer showTimer;
+
+        List<string> userPhrases;
+
+
+        public ReminderWindow()
+        {
+            InitializeComponent();
+            userPhrases = ((MainWindow)Application.Current.MainWindow).Phrases;
+        }
+
+        private void userImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //accessing value of image path from MainWindow
+            string testPath = ((MainWindow)Application.Current.MainWindow).ImagePath;
+            try
+            {
+                userImage.Source = new BitmapImage(new Uri(testPath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var screenWidth = SystemParameters.VirtualScreenWidth;
+            Left = screenWidth - 425;
+            Top = 50;
+
+            Visibility = Visibility.Collapsed;
+
+            showTimer = new DispatcherTimer();
+            showTimer.Interval = TimeSpan.FromSeconds(5);
+            showTimer.Tick += charTimer_Tick;
+            showTimer.Start();
+
+        }
+
+        void charTimer_Tick(object sender, EventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Visibility = Visibility.Collapsed;
+                showTimer.Interval = isHidden;
+            }
+            else
+            {
+                phrasesTextBlock.Text = GetNextPhrase();
+                Visibility = Visibility.Visible;
+                showTimer.Interval = isShowing;
+            }
+        }
+
+        private void phrasesTextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private string GetNextPhrase()
+        {
+            currentPhrase++;
+            if (currentPhrase > userPhrases.Count - 1)
+            {
+                currentPhrase = 0;
+            }
+
+            return userPhrases[currentPhrase];
+        }
+    }
+}
